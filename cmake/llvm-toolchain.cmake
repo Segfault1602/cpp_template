e90ca061704ce0af39b64712423e1e4144d6b48b
Alex St-Onge <stonge.alex@gmail.com>
2026-05-09 10:16:09 -0700

    misc cleanup

--- cmake/llvm-toolchain.cmake
+++ cmake/llvm-toolchain.cmake
@@ -1,12 +1,7 @@
 include_guard(GLOBAL)
 
-if(APPLE)
-  set(CMAKE_CXX_COMPILER "/opt/homebrew/opt/llvm@20/bin/clang++")
-  set(CMAKE_C_COMPILER "/opt/homebrew/opt/llvm@20/bin/clang")
-else()
-  set(CMAKE_CXX_COMPILER "clang++")
-  set(CMAKE_C_COMPILER "clang")
-endif()
+set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_C_COMPILER "clang")
 
 # For openmp support on Windows
 if(WIN32)
--- cmake/project-warnings.cmake
+++ cmake/project-warnings.cmake
@@ -7,7 +7,10 @@ elseif(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
       -Wpedantic
       -Wno-sign-compare
       -Wno-language-extension-token
-      #   -Wunsafe-buffer-usage
-  )
+      -Wunsafe-buffer-usage)
 
 endif()
+
+add_library(myproject_warnings INTERFACE)
+add_library(myproject::myproject_warnings ALIAS myproject_warnings)
+target_compile_options(myproject_warnings INTERFACE ${MYPROJECT_WARNINGS_CXX})
